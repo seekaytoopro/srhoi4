79ed726d50ce9dcf0cbfeac9d0249aa37238b4d8
seekaytoopro <seekaytoopro@gmail.com>
2021-06-09 07:52:49 -0500

    Large buff to anti-sub warfare. Cruisers and Destroyers now have much more base depth charges and sub detection. The hit profile of depth charges decreased from 100 to 80. Airplane catapaults give 1.0 depth charges, and multiply the depth charges of the ship. Airplane catapaults and sonar give increased sub detection. Sub hulls have slightly higher visiblity. Might need to be lowered again in the future, but for now, this should help to stop the sub spam getting convoys stuck in a zone in an endless naval battle, and you will be rewarded more for creating an anti sub fleet

--- common/defines/sr_defines.lua
+++ common/defines/sr_defines.lua
@@ -479,7 +479,7 @@ NDefines.NNavy.CONVOY_EFFICIENCY_REGAIN_BASE_SPEED = 0.04						-- How much effic
 NDefines.NNavy.CONVOY_EFFICIENCY_MIN_VALUE = 0.04								-- To avoid complete 0% efficiency, set the lower limit.
 NDefines.NNavy.CONVOY_ATTACK_BASE_FACTOR = 0.25                             	-- base % of convoys that get intercepted
 
-NDefines.NNavy.DEPTH_CHARGES_HIT_PROFILE = 100
+NDefines.NNavy.DEPTH_CHARGES_HIT_PROFILE = 80
 NDefines.NNavy.BASE_JOIN_COMBAT_HOURS = 2 										-- the taskforces that wants to join existing combats will wait for at least this amount 8--->2 
 NDefines.NNavy.DEPTH_CHARGES_HIT_CHANCE_MULT = 10.0 								-- multiplies hit chance of small guns, early application of 1.6.2 
 NDefines.NNavy.DEPTH_CHARGES_DAMAGE_MULT = 1.0 									-- early application of 1.6.2 
